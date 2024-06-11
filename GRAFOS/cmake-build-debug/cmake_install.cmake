39b4ca61182450d8efb9775b4cdb78eb71047555
JoseCarioca <65904569+JoseCarioca@users.noreply.github.com>
2024-06-11 21:15:26 +0200

    add ignore

--- GRAFOS/cmake-build-debug/cmake_install.cmake
+++ GRAFOS/cmake-build-debug/cmake_install.cmake
@@ -34,7 +34,7 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "C:/Users/epicg/AppData/Local/JetBrains/Toolbox/apps/CLion/ch-0/233.14475.31/bin/mingw/bin/objdump.exe")
+  set(CMAKE_OBJDUMP "C:/Users/epicg/AppData/Local/Programs/CLion/bin/mingw/bin/objdump.exe")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
